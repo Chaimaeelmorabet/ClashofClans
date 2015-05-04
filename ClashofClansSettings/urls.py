fe00d265e3e4601c4faf799e248b6aa2876ff243
pestomerdes <albertmerino11@gmail.com>
2015-05-05 00:00:18 +0200

    Tret el exemple myrestaurants del sistema

--- ClashofClansSettings/urls.py
+++ ClashofClansSettings/urls.py
@@ -8,6 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    (r'^myrestaurants/',  include('myrestaurants.urls',  namespace='myrestaurants')),
     (r'^ClashofClans/',  include('ClashofClans.urls',  namespace='ClashofClans')),
 )
--- myrestaurants/admin.py
+++
@@ -1,6 +0,0 @@
-import models 
-from django.contrib	import admin
-  
-admin.site.register(models.Restaurant)	  
-admin.site.register(models.Dish)	  
-admin.site.register(models.RestaurantReview)
