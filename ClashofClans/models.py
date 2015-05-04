8d4ea6e8f64c718100ac627ea6056844f47a6a19
pestomerdes <albertmerino11@gmail.com>
2015-05-04 14:13:49 +0200

    Creades relacions entre entitats

--- ClashofClans/models.py
+++ ClashofClans/models.py
@@ -8,7 +8,6 @@ class Clan(models.Model):
     tipusOpciones = ((1,'Privado'),(2,'Publico'),(3,'Solo invitacion'))
     nom = models.CharField(max_length=60)
     idClan = models.AutoField(primary_key=True)
-    idGuerra = models.IntegerField()
     punts = models.IntegerField(default=0)
     tipus = models.PositiveSmallIntegerField('Tipo clan', blank=False, default=2, choices=tipusOpciones)
     trofeusBase = models.IntegerField(default=0)
@@ -17,47 +16,47 @@ class Clan(models.Model):
         return self.nom
 
 
-class Ciutat(models.Model):
-    id = models.IntegerField(primary_key=True)
-    def __unicode__(self):
-        return str(self.id)
-    def get_absolute_url(self):
-        return reverse('ClashofClans:ciutat_list', kwargs={})
-
-class Jugador(models.Model):
-    nom = models.CharField(max_length=60)
-    id = models.AutoField(primary_key=True)
-    nivell= models.IntegerField(default=0)
-    idLliga = models.IntegerField(default=0)
-    idClan = models.IntegerField(default=0)
-    idCiutat = models.IntegerField()
-    def __unicode__(self):
-        return self.nom
-    def get_absolute_url(self):
-        return reverse('ClashofClans:ciutat_list', kwargs={})
-
-
-class Lligue(models.Model):
-    idLliga = models.IntegerField(default=0)
-    idPremi = models.IntegerField(default=0)
-    numCopes = models.IntegerField(default=0);
-    def __unicode__(self):
-        return str(self.idLliga)
-
-class Guerra(models.Model):
-    idGuerra = models.IntegerField()
-    idClan1 = models.IntegerField()
-    idClan2 = models.IntegerField()
-    def __unicode__(self):
-        str(self.idGuerra)
-
-
-
-
 class PremiLligue(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     oro = models.IntegerField()
     elixir = models.IntegerField()
     elixirNegre = models.IntegerField()
     def __unicode__(self):
         return str(self.id)
+
+class Lligue(models.Model):
+    idLliga = models.AutoField(primary_key=True)
+    idPremi = models.ForeignKey(PremiLligue, null=True)
+    numCopes = models.IntegerField(default=0)
+    def __unicode__(self):
+        return str(self.idLliga)
+
+class Jugador(models.Model):
+    nom = models.CharField(max_length=60)
+    id = models.AutoField(primary_key=True)
+    nivell= models.IntegerField(default=0)
+    lliga = models.ForeignKey(Lligue, null=True,blank=True)
+    clan = models.ForeignKey(Clan, null=True,blank=True)
+    def __unicode__(self):
+        return self.nom
+    def get_absolute_url(self):
+        return reverse('ClashofClans:ciutat_list', kwargs={})
+
+class Guerra(models.Model):
+    idGuerra = models.AutoField(primary_key=True)
+    idClan1 = models.ForeignKey(Clan, null=True, related_name='idClans1')
+    idClan2 = models.ForeignKey(Clan, null=True, related_name='idClans2')
+    def __unicode__(self):
+        str(self.idGuerra)
+
+
+class Ciutat(models.Model):
+    id = models.AutoField(primary_key=True)
+    jugador = models.ForeignKey(Jugador, null=True, related_name='ciutats')
+
+    def __unicode__(self):
+        return str(self.id)
+    def get_absolute_url(self):
+        return reverse('ClashofClans:ciutat_list', kwargs={})
+
+
